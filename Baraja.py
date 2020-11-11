ebf14e8ec2e130ce59c75161d13b042394d98874
esther <efr78@hotmail.com>
2020-11-11 22:35:23 +0100

    Barajar Baraja

--- Baraja.py
+++ Baraja.py
@@ -1,15 +1,35 @@
+import random
+
 palos= ['o','c','e','b']
 numeros= ['A','2','3','4','5','6','7','S','C','R']
-
-
 
 def creaBaraja():
     baraja=[]
     for palo in palos:
         for numero in numeros:
             baraja.append(numero+palo)
-    return (baraja)
+    return baraja
 
-creaBaraja()
+'''
+def intercambio(primer_valor,segundo_valor):
+    aux= primer_valor
+    primer_valor= segundo_valor
+    segundo_valor= aux
+    return primer_valor,segundo_valor
+'''
+
+def barajar(lista_de_naipes):
+    for i in range(len(lista_de_naipes)):
+        nueva_pos= random.randrange(len(lista_de_naipes))
+        '''
+        intercambio de cartas tecnica vaso vacio'''
+        aux= lista_de_naipes[nueva_pos]
+        lista_de_naipes[nueva_pos]=lista_de_naipes[i]
+        lista_de_naipes[i]=aux
+    return lista_de_naipes 
+
+
+
+
 
     
