a2198302cdbe6b461e28fc5bab5aacbb6f51e9b5
esther <efr78@hotmail.com>
2020-11-12 22:09:41 +0100

    baraja final con repartir

--- barajaC.py
+++ barajaC.py
@@ -5,7 +5,22 @@ class MiBaraja():
     def __init__(self,palos, numeros):
         self.palos= palos
         self.numeros= numeros
-        self.mazacote=Baraja.creaBaraja(palos,numeros)
+        self.mazacote= Baraja.creaBaraja(palos,numeros)
         
     def barajar(self):
         Baraja.barajar(self.mazacote)
+
+    def repartir(self,num_jugadores, num_cartas):
+        jugadores =[]
+        for i in range(num_jugadores):
+            jugadores.append([])
+
+
+        for carta in range(num_cartas):
+            for jugador in range(num_jugadores):
+                #carta=self.mazacote.pop()#aqui guarda la carta sacada con pop.
+                #jugadores[jugador].append(carta)
+                jugadores[jugador].append(self.mazacote.pop())
+        return jugadores 
+
+#pop extrae el elemento del indice. Si no lo pones quita la ultima.    
--- main.py
+++ main.py
@@ -5,9 +5,14 @@ numeros= ['A','2','3','4','5','6','7','S','C','R']
 
 miBaraja= barajaC.MiBaraja(palos,numeros)
 
+miBaraja.mazacote[2]
+
 print(miBaraja.mazacote)
 
+print(miBaraja.repartir(3,5))
 
+miBaraja.barajar()
+print(miBaraja.mazacote)
 
 
 
